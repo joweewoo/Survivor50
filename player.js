af72eadb47d2efd351ec57bdff55b66a7a89e987
joweewoo <josephplamorte@gmail.com>
2026-02-21 13:22:39 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-const weeks = ["episode2.json"];
+const episodes = ["episode2.json"]; // add more later
 
 const params = new URLSearchParams(window.location.search);
 const playerName = params.get("name");
@@ -10,35 +10,42 @@ let totalScore = 0;
 async function loadData() {
   const container = document.getElementById("weeklyBreakdown");
 
-  for (let file of weeks) {
+  for (let file of episodes) {
     const response = await fetch(`data/${file}`);
     const data = await response.json();
 
-    const weekDiv = document.createElement("div");
-    weekDiv.innerHTML = `<h3>Episode ${data.week}</h3>`;
+    const episodeDiv = document.createElement("div");
+    episodeDiv.innerHTML = `<h3>Episode ${data.episode}</h3>`;
 
-    const actions = data.scores[playerName];
+    const playerArray = data.matrix[playerName];
 
-    if (actions && actions.length > 0) {
-      const ul = document.createElement("ul");
-
-      actions.forEach(item => {
-        totalScore += item.points;
-
-        const li = document.createElement("li");
-        li.innerText = `${item.action} (+${item.points})`;
-        ul.appendChild(li);
-      });
-
-      weekDiv.appendChild(ul);
-    } else {
-      weekDiv.innerHTML += "<p>No points this episode.</p>";
+    if (!playerArray) {
+      episodeDiv.innerHTML += "<p>No data.</p>";
+      container.appendChild(episodeDiv);
+      continue;
     }
 
-    container.appendChild(weekDiv);
+    const ul = document.createElement("ul");
+
+    playerArray.forEach((points, index) => {
+      if (points !== 0) {
+        totalScore += points;
+
+        const li = document.createElement("li");
+        li.innerText = `${data.actions[index]} (+${points})`;
+        ul.appendChild(li);
+      }
+    });
+
+    if (ul.children.length === 0) {
+      episodeDiv.innerHTML += "<p>No points this episode.</p>";
+    } else {
+      episodeDiv.appendChild(ul);
+    }
+
+    container.appendChild(episodeDiv);
   }
 
   document.getElementById("totalScore").innerText = totalScore;
 }
-
 loadData();
